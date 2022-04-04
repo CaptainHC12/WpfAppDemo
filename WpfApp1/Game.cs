257168d8e0fd973c76a3b7cc4b37294884d4bcd0
CaptainHC12 <96395148+CaptainHC12@users.noreply.github.com>
2022-04-04 15:06:36 -0500

    Add project files.

---
+++ WpfApp1/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+   public class Game
+    {
+        public string GameName = "My Super Great Game";
+
+
+        public Person Player = new Person
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/Welcome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Welcome.xaml
+    /// </summary>
+    public partial class Welcome : Page
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("MainGameInterface.xaml", UriKind.Relative));
+        }
+
+        
+    }
+}
